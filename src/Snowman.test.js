a711e12d81e2eeea22fce9a8dd5b86a152a1a57d
Daniel Zych <danjzych@gmail.com>
2023-09-27 14:19:29 -0500

    Completed refactor for DOM bugs and wrote tests

--- src/Snowman.test.js
+++ src/Snowman.test.js
@@ -1,19 +1,38 @@
 import { render, fireEvent } from "@testing-library/react";
 import Snowman from "./Snowman";
 
-it("image does not disappear after loss condition" ,function(){
-  const { container } = render(
-    <Snowman />)
+it("matches previous snapshot", function () {
+  const { container } = render(<Snowman />);
 
-    const img = container.querySelector("img");
+  expect(container).toMatchSnapshot();
+});
 
-    fireEvent.click(container.querySelector("button[value=b]"));
-    fireEvent.click(container.querySelector("button[value=c]"));
-    fireEvent.click(container.querySelector("button[value=d]"));
-    fireEvent.click(container.querySelector("button[value=f]"));
-    fireEvent.click(container.querySelector("button[value=g]"));
-    fireEvent.click(container.querySelector("button[value=h]"));
-    fireEvent.click(container.querySelector("button[value=z]"));
+it("image does not disappear after loss condition", function () {
+  const { container } = render(<Snowman />);
 
-    expect(img.getAttribute("src")).toContain(".png")
-})
+  const img = container.querySelector("img");
+
+  fireEvent.click(container.querySelector("button[value=b]"));
+  fireEvent.click(container.querySelector("button[value=c]"));
+  fireEvent.click(container.querySelector("button[value=d]"));
+  fireEvent.click(container.querySelector("button[value=f]"));
+  fireEvent.click(container.querySelector("button[value=g]"));
+  fireEvent.click(container.querySelector("button[value=h]"));
+  fireEvent.click(container.querySelector("button[value=z]"));
+
+  expect(img.getAttribute("src")).toContain(".png");
+});
+
+it("displays 'you lose!!' when losing game", function () {
+  const { container } = render(<Snowman />);
+
+  fireEvent.click(container.querySelector("button[value=b]"));
+  fireEvent.click(container.querySelector("button[value=c]"));
+  fireEvent.click(container.querySelector("button[value=d]"));
+  fireEvent.click(container.querySelector("button[value=f]"));
+  fireEvent.click(container.querySelector("button[value=g]"));
+  fireEvent.click(container.querySelector("button[value=h]"));
+  fireEvent.click(container.querySelector("button[value=z]"));
+
+  expect(container).toHaveTextContent("You lose!!!");
+});
